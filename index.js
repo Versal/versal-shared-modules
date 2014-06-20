5f7d1bba4f370d6f752c4e69f160bf25eb50301e
Shaohua Zhou <shao@versal.com>
2014-06-20 14:12:21 -0700

    Add classList shim

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
 module.exports = {
-  'vs.sanitize': require('./vs.sanitize/dist/vs.sanitize')
+  'vs.sanitize': require('./vs.sanitize/dist/vs.sanitize'),
+  'vs.classlist': require('./vs.classlist/vs.classlist')
 };
