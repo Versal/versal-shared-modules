a0aa784efcab54b20ba7615fdea3bc276032962e
Shaohua Zhou <shao@versal.com>
2014-06-20 17:10:26 -0700

    Add basecompmethods
    Add an URL pointing to the exact sha for the lib
    
    fix indentation
    
    Move url to the top

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 module.exports = {
   'vs.sanitize': require('./vs.sanitize/dist/vs.sanitize'),
-  'vs.classlist': require('./vs.classlist/vs.classlist')
+  'vs.classlist': require('./vs.classlist/vs.classlist'),
+  'vs.basecompmethods': require('./vs.basecompmethods/vs.basecompmethods')
 };
