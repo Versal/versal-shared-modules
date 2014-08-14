c0998bfaf38330d66d9fe4b72bfdd5eae1929de4
shaohua <sh.zhou@gmail.com>
2014-08-13 18:07:44 -0700

    Add a special case for the sdk
    Export BaseCompMethods to window even when there is amd

--- vs.basecompmethods/vs.basecompmethods.js
+++ vs.basecompmethods/vs.basecompmethods.js
@@ -1,15 +1,18 @@
 (function (root, factory) {
-    if (typeof define === 'function' && define.amd) {
-        // AMD. Register as an anonymous module.
-        define([], factory);
-    } else if (typeof exports === 'object') {
-        // Node. Does not work with strict CommonJS, but
-        // only CommonJS-like environments that support module.exports,
-        // like Node.
-        module.exports = factory();
-    } else {
-        // Browser globals (root is window)
-        root.BaseCompMethods = factory();
+  if (typeof define === 'function' && define.amd) {
+    // AMD. Register as an anonymous module.
+    define([], factory);
+    // SPECIAL case, versal-sdk has require in the env but stil need Browser globals
+    // Browser globals (root is window)
+    root.BaseCompMethods = factory();
+  } else if (typeof exports === 'object') {
+    // Node. Does not work with strict CommonJS, but
+    // only CommonJS-like environments that support module.exports,
+    // like Node.
+    module.exports = factory();
+  } else {
+    // Browser globals (root is window)
+    root.BaseCompMethods = factory();
   }
 }(this, function () {
 
